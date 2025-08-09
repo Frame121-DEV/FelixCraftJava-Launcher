8317b134d73eaf6d7ef83151ed5b42700a91d951
Frame121 <framef795@gmail.com>
2025-08-09 14:27:01 +0700

    UpdateL2

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://frame121-dev.github.io/FelixCraftJava-Distro//distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://frame121-dev.github.io/FelixCraftJava-Distro/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
